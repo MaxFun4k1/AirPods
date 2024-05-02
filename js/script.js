f8df18e2d6cdba2c83b48c562c2813a9d0f12c3c
Max Kuznecov <maks-kuznecov11@mail.ru>
2024-05-02 14:40:35 +0300

    create landing AirPods

---
+++ js/script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const chooseColor = document.querySelectorAll('.choose__color-btn');
+const contentItem = document.querySelectorAll('.content-item');
+
+chooseColor.forEach(function(element) {
+	element.addEventListener('click', open);
+});	
+
+function open(evt) {
+	const target = evt.currentTarget;
+	const button = target.dataset.button;
+	const contentActive = document.querySelectorAll(`.${button}`);
+
+	chooseColor.forEach(function(item) {
+		item.classList.remove('choose__color-btn--active');
+	});
+
+	target.classList.add('choose__color-btn--active');
+
+	contentItem.forEach(item => {
+		item.classList.remove('content-item__active');
+	});
+
+	contentActive.forEach(item => {
+		item.classList.add('content-item__active');
+	});
+}
